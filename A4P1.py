8b882f19c3a1570a9607fe97a30884cf3371ad81
Jacob Nguyen <jvnguyen@cs.ualberta.ca>
2021-03-19 14:21:47 -0600

    added query with indexing code

--- A4P1.py
+++ A4P1.py
@@ -16,7 +16,8 @@
 import sqlite3
 import time
 import random
-
+connection = None
+cursor = None
 DATABASE_INFO = [('./A4v100.db', 100), 
                        ('./A4v1k.db', 1000), 
                        ('./A4v10k.db', 10000), 
@@ -29,104 +30,140 @@ upc_codes = []
 #The below two functions do the same thing (differ in performance for some reason), but the hardcoded is easier to read
 
 def runQueriesHardCoded():
-    Q1 = """SELECT partPrice FROM Parts WHERE partNumber=?"""
-    Q2 = """SELECT partPrice FROM Parts WHERE needsPart=?"""
-    
-    for name, cardinality in DATABASE_INFO:
+	global connection, cursor
+	Q1 = """SELECT partPrice FROM Parts WHERE partNumber=?"""
+	
+	Q2 = """SELECT partPrice FROM Parts WHERE needsPart=?"""
+	for name, cardinality in DATABASE_INFO:
         # Connect to each database in the table
-        connection = sqlite3.connect(name)
-        cursor = connection.cursor()
+		connection = sqlite3.connect(name)
+		cursor = connection.cursor()
         
         ############################# Start of first query #############################
         
         #start timer
-        time_start = time.perf_counter()
+		time_start = time.perf_counter()
         
-        # Run first query 100 times
-        for numExecutions in range(0,100):
-            randPartNumber = random.choice(upc_codes)  
-            cursor.execute(Q1, (randPartNumber,))
+        # Run first query 100 times	
+		for numExecutions in range(0,100):
+			randPartNumber = random.choice(upc_codes)  
+			cursor.execute(Q1, (randPartNumber,))
             
-        connection.commit()
+		connection.commit()
         
         #end timer and calculate difference between times
-        time_end = time.perf_counter()
-        avg_time = ((time_end-time_start)*1000)
-        print("Average time to run Q1 100 times in database {}: {} milliseconds ({} millieseconds per query)".format(name, avg_time, avg_time/100))
+		time_end = time.perf_counter()
+		avg_time = ((time_end-time_start)*1000)
+		print("Average time to run Q1 100 times in database {}: {} milliseconds ({} millieseconds per query)".format(name, avg_time, avg_time/100))
         ############################# End of first query #############################
-        
+		 
         ############################# Start of second query #############################
         
         #start timer
-        time_start = time.perf_counter()
+		time_start = time.perf_counter()
         
         # Run second query 100 times
-        for numExecutions in range(0,100):
-            randPartNumber = random.choice(upc_codes)  
-            cursor.execute(Q2, (randPartNumber,))
+		for numExecutions in range(0,100):
+			randPartNumber = random.choice(upc_codes)	
+			cursor.execute(Q2, (randPartNumber,))
             
-        connection.commit()
+		connection.commit()
         
         #ebd tuner abd calculate difference between times
-        time_end = time.perf_counter()
-        avg_time = ((time_end-time_start)*1000)
-        print("Average time to run Q1 100 times in database {}: {} milliseconds ({} millieseconds per query)".format(name, avg_time, avg_time/100))
-        connection.commit()
+		time_end = time.perf_counter()
+		avg_time = ((time_end-time_start)*1000)
+		print("Average time to run Q1 100 times in database {}: {} milliseconds ({} millieseconds per query)".format(name, avg_time, avg_time/100))		
+		connection.commit()
         ############################# End of second query #############################
-        
+	
         # Close db connection, loop back to top and open next database
-        connection.close()
+		connection.close()
     
-
+def runQueryIndex():
+	global connection, cursor
+	createIndex = ("CREATE INDEX IF NOT EXISTS idxNeedsPart ON Parts(needsPart);")
+#cursor.execute(createIndex)
+	Query1 = """SELECT partPrice FROM Parts WHERE partNumber=?"""
+	Query2 = """SELECT partPrice FROM Parts WHERE needsPart=?"""
+	
+	for name, cardinality in DATABASE_INFO:	
+		connection = sqlite3.connect(name)
+		cursor = connection.cursor()
+		cursor.execute(createIndex)
+		startTimeIndex = time.perf_counter()
+		
+		for numRunsIndex in range(0, 100):
+			randPartNumber = random.choice(upc_codes)
+			cursor.execute(Query1, (randPartNumber,))
+		connection.commit()
+		endTimeIndex = time.perf_counter()
+		avgTimeIndex = ((endTimeIndex-startTimeIndex)*1000)
+		print("Average time to run Query 1 100 times in database {}: {} millieseconds ({}milliseconds per query)".format(name, avgTimeIndex, avgTimeIndex/100))
+##########################
+		startTimeIndex = time.perf_counter()
+		for numRunsIndex in range(0, 100):
+			randPartNumber = random.choice(upc_codes)
+			cursor.execute(Query2, (randPartNumber,))
+		connection.commit()
+		endTimeIndex = time.perf_counter()
+		avgTimeIndex = ((endTimeIndex-startTimeIndex)*1000)
+		print("Average time to run Query 1 100 times in database {}: {} millieseconds ({}milliseconds per query)".format(name, avgTimeIndex, avgTimeIndex/100))
+		connection.commit()
+		connection.close()
+"""
 def runQueries():
+	global connection, cursor
     #Q1: Given a randomly selected UPC code U from the UPC database find the price of part in Parts that has partNumber = U
     #Q2: Given a randomly selected UPC code U from the UPC database find the price of part in Parts that has needsPart = U
-    queries = ["SELECT partPrice FROM Parts WHERE partNumber=?", "SELECT partPrice FROM Parts WHERE needsPart=?"]
+	queries = ["SELECT partPrice FROM Parts WHERE partNumber=?", "SELECT partPrice FROM Parts WHERE needsPart=?"]
     
     #Run each query 100 times (each time using a randomly generated value for U) against each version of table Parts and record the average query time required for answering both queries using each version of table Parts.
-    for name, cardinality in DATABASE_INFO:
-        connection = sqlite3.connect(name)
-        cursor = connection.cursor()
+	for name, cardinality in DATABASE_INFO:
+		connection = sqlite3.connect(name)
+		cursor = connection.cursor()
         
-        for query in queries:
-            time_start = time.perf_counter()
-            for numExecutions in range(0,100):
-                randPartNumber = random.choice(upc_codes)  
-                cursor.execute(query, (randPartNumber,))
-                val = cursor.fetchone()
-            connection.commit()
-            time_end = time.perf_counter()
-            avg_time = ((time_end-time_start)*1000)
-            print("Average time to run {} 100 times in database {}: {} milliseconds ({} millieseconds per query)".format(query, name, avg_time, avg_time/100))
-            
-        connection.commit()
-        connection.close()
-
+		for query in queries:
+			time_start = time.perf_counter()
+			for numExecutions in range(0,100):
+				randPartNumber = random.choice(upc_codes)  
+				cursor.execute(query, (randPartNumber,))
+				val = cursor.fetchone()
+			connection.commit()
+			time_end = time.perf_counter()
+			avg_time = ((time_end-time_start)*1000)
+			print("Average time to run {} 100 times in database {}: {} milliseconds ({} millieseconds per query)".format(query, name, avg_time, avg_time/100))
+		connection.commit()
+		connection.close()
+"""
 def loadUPC():
-    connection = sqlite3.connect("./UPC.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM upc")
-    for value in cursor.fetchall():
-        upc_code = value[0]
-        upc_codes.append(upc_code)
-    connection.commit()
-    connection.close()
+	global connection, cursor
+	connection = sqlite3.connect("./UPC.db")
+	cursor = connection.cursor()
+	cursor.execute("SELECT * FROM upc")
+	for value in cursor.fetchall():
+		upc_code = value[0]
+		upc_codes.append(upc_code)
+	connection.commit()
+	connection.close()
     
 def loadCountries():
-    connection = sqlite3.connect("./Country.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM country")
-    for value in cursor.fetchall():
-        country_code = value[0]
-        countries.append(country_code)
-    connection.commit()
-    connection.close()        
+	global connection, cursor
+	connection = sqlite3.connect("./Country.db")
+	cursor = connection.cursor()
+	cursor.execute("SELECT * FROM country")
+	for value in cursor.fetchall():
+		country_code = value[0]
+		countries.append(country_code)
+	connection.commit()
+	connection.close()        
 
 def main():
-    print("Executing Part 1")
-    loadCountries()
-    loadUPC()
-    runQueriesHardCoded()
-    
+	global connection, cursor
+	print("Executing Part 1, task 1")
+	loadCountries()
+	loadUPC()
+	runQueriesHardCoded()
+	print("Executing Part1, task 2")
+	runQueryIndex()
 
 main()
