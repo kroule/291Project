a41551933f069a136738bd61ce4053219787dfea
Jacob Nguyen <jvnguyen@cs.ualberta.ca>
2021-03-19 21:25:31 -0600

    finished task 6

--- A4P2.py
+++ A4P2.py
@@ -4,18 +4,58 @@ import time
 connection = None
 cursor = None
 
-def connect(path):
-    global connection, cursor
+DATABASE_INFO = [('./A4v100.db', 100), ('./A4v1k.db', 1000),('./A4v10k.db', 10000), ('./A4v100k.db', 100000), ('./A4v1M.db',1000000)]
 
-    connection = sqlite3.connect(path)
-    
-    cursor = connection.cursor()
-    cursor.execute(' PRAGMA foreign_keys=ON; ')
-    connection.commit()
-    return
+#upc_codes = []
+#countries = []
 
+def runQuery():
+	global connection, cursor
+	Query = """SELECT AVG(partPrice) FROM Parts GROUP BY madeIn"""
+	dropIndex = ("DROP INDEX IF EXISTS idxNeedsParts;")
+
+	for name, cardinality in DATABASE_INFO:
+		connection = sqlite3.connect(name)
+		cursor = connection.cursor()
+		cursor.execute(dropIndex)
+
+		startTimer = time.perf_counter()
+		for queryExecution in range(0, 100):
+			cursor.execute(Query)
+
+		connection.commit()
+		stopTimer = time.perf_counter()
+		avgQueryTime = ((stopTimer - startTimer)*1000)
+		print("Average time to run query 100 times in datatbase {}: {} milliseconds ({} milliseconds per query)".format(name, avgQueryTime, avgQueryTime/100))
+
+		connection.close()
+
+
+"""
+def loadUPC():
+	global connection, cursor
+	connection = sqlite3.connect("./UPC.db")
+	cursor = connection.cursor()
+	cursor.execute("SELECT * FROM upc")
+	for value in cursor.fetchall():
+		upc_code = value[0]
+		upc_codes.append(upc_code)
+	connection.commit()
+	connection.close()
+
+def loadCOuntries():
+	global connection, cursor
+	connection = sqlite3.connect("./Country.db")
+	cursor = connection.cursor()
+	cursor.execute("SELECT * FROM country")
+	for value in cursor.fetchall():
+		country_code = value[0]
+		countries.append(country_code)
+	connection.commit()
+	connection.close() 
+"""
 def main():
-    global connection, cursor
-	printf("Executing Part 2, task")
-	
+	global connection, cursor
+
+	runQuery()
 main()
